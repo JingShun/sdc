77b1330727d86e24dc066cf13ba23bf4a51cbc90
chaos.fractal <chaos.fractal@chtsecurity.com>
2024-08-04 17:58:42 +0800

    調整資安院VANS增加填寫改善措施的填寫率

--- controller/vans/vans_nics_cpe_progress.php
+++ controller/vans/vans_nics_cpe_progress.php
@@ -1,6 +1,7 @@
 <?php
 $sql = "SELECT
         nics_vans_asset.unit_name,
+        security_rank.rank,
         nics_vans_asset.product_type,
         nics_vans_asset.asset_vendor,
         nics_vans_asset.asset_name,
@@ -9,7 +10,8 @@ $sql = "SELECT
         Sum(nics_vans_asset.cve_number) AS cve_number,
         Sum(nics_vans_asset.cve_number - nics_vans_asset.progress_number) AS no_progress_number
     FROM `nics_vans_asset`
-    WHERE cve_number <> progress_number
+    LEFT JOIN security_rank ON nics_vans_asset.unit_name = security_rank.`name`
+    WHERE cve_number > 0
     GROUP BY unit_name, product_type, asset_name, asset_vendor
     ORDER BY unit_name, product_type, asset_vendor";
 $queryNotProgressAsset = $db->execute($sql);
@@ -18,10 +20,11 @@ $nicsOuUpdatedAt = 0;
 $ou_list = array();
 $nicsOuRows = array();
 foreach ($queryNotProgressAsset as $asset) {
-    if (!isset($nicsOuRows[$asset['unit_name'] . $asset['product_type']])) {
-        $nicsOuRows[$asset['unit_name'] . $asset['product_type']] = array(
+    if (!isset($nicsOuRows[$asset['unit_name'] . 'AnyType'])) {
+        $nicsOuRows[$asset['unit_name']  . 'AnyType'] = array(
             'unit_name' => $asset['unit_name'],
-            'product_type' => $asset['product_type'],
+            'rank' => $asset['rank'],
+            'product_type' => 'AnyType',
             'asset_number' => 0,
             'cvss' => $asset['cvss'],
             'cve_number' => 0,
@@ -29,15 +32,24 @@ foreach ($queryNotProgressAsset as $asset) {
         );
     }
 
-    $nicsOuRows[$asset['unit_name'] . $asset['product_type']]['asset_number'] += $asset['asset_number'];
-    $nicsOuRows[$asset['unit_name'] . $asset['product_type']]['cve_number'] += $asset['cve_number'];
-    $nicsOuRows[$asset['unit_name'] . $asset['product_type']]['no_progress_number'] += $asset['no_progress_number'];
-    $nicsOuRows[$asset['unit_name'] . $asset['product_type']]['cvss'] = max($nicsOuRows[$asset['unit_name'] . $asset['product_type']]['cvss'], $asset['cvss']);
+    $nicsOuRows[$asset['unit_name'] . 'AnyType']['asset_number'] += $asset['asset_number'];
+    $nicsOuRows[$asset['unit_name'] . 'AnyType']['cve_number'] += $asset['cve_number'];
+    $nicsOuRows[$asset['unit_name'] . 'AnyType']['no_progress_number'] += $asset['no_progress_number'];
+    $nicsOuRows[$asset['unit_name'] . 'AnyType']['cvss'] = max($nicsOuRows[$asset['unit_name'] . 'AnyType']['cvss'], $asset['cvss']);
+
+    $totalCVE = $nicsOuRows[$asset['unit_name'] . 'AnyType']['cve_number'];
+
+    $nicsOuRows[$asset['unit_name'] . 'AnyType']['rate'] = round(100 * ($totalCVE - $nicsOuRows[$asset['unit_name'] . 'AnyType']['no_progress_number']) / $totalCVE, 2);
+
 
     $ou_list[$asset['unit_name']] = 1;
 }
 
+// 依照修補率高到低排序
+usort($nicsOuRows, fn ($a, $b) => $a['rate'] < $b['rate'] ? 1 : -1);
 
+// 尚未填寫完畢機關數
+$notFinishOuCount = count(array_filter($nicsOuRows, fn ($ou) => $ou['no_progress_number']));
 
 require 'view/header/default.php';
 require 'view/body/vans/vans_nics_cpe_progress.php';
--- view/body/vans/vans_nics_cpe_progress.php
+++ view/body/vans/vans_nics_cpe_progress.php
@@ -15,27 +15,40 @@
                             <?php if (count($nicsOuRows) == 0) : ?>
                                 <p>查無此筆紀錄</p>
                             <?php else : ?>
-                                <p>共有 <?= count($ou_list) ?> 個機關的改善措施尚未填寫完畢！</p>
+                                <p>共有 <?= $notFinishOuCount ?> 個機關的改善措施尚未填寫完畢！</p>
                                 <table class="ui celled table">
                                     <thead>
                                         <tr>
                                             <th>ou</th>
-                                            <th>類別</th>
+                                            <th>level</th>
+                                            <!-- <th>類別</th> -->
                                             <th>資產數量</th>
                                             <!-- <th>CVSS</th> -->
                                             <th>CVE數量</th>
                                             <th>未填改善措施數量</th>
+                                            <th>填寫率(%)</th>
+                                            <th width="40%"></th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <?php foreach ($nicsOuRows as $row) : ?>
                                             <tr>
                                                 <td><?= $row['unit_name'] ?></td>
-                                                <td><?= $row['product_type']=='system' ? '資通系統':'PC' ?></td>
+                                                <td><?= $row['rank'] ?></td>
+                                                <!-- <td><?= $row['product_type'] == 'system' ? '資通系統' : ($row['product_type'] == 'user' ? 'PC' : $row['product_type']) ?></td> -->
                                                 <td><?= $row['asset_number'] ?></td>
                                                 <!-- <td><?= $row['cvss'] ?></td> -->
                                                 <td><?= $row['cve_number'] ?></td>
                                                 <td><?= $row['no_progress_number'] ?></td>
+                                                <td><?= $row['rate'] ?></td>
+                                                <td>
+                                                    <div class='ui teal progress yckuo' data-sort-value='<?= $row['rate'] ?>' data-percent='<?= $row['rate'] ?>' data-total='100'>
+                                                        <div class='bar'>
+                                                            <div class='progress'></div>
+                                                        </div>
+                                                        <div class='label'><?= $row['cve_number'] - $row['no_progress_number'] ?>/<?= $row['cve_number'] ?></div>
+                                                    </div>
+                                                </td>
                                             </tr>
                                         <?php endforeach ?>
                                     </tbody>
@@ -46,7 +59,7 @@
                             <?php if (count($queryNotProgressAsset) == 0) : ?>
                                 <p>查無此筆紀錄</p>
                             <?php else : ?>
-                                
+
                                 <table class="ui celled table">
                                     <thead>
                                         <tr>
@@ -64,7 +77,7 @@
                                         <?php foreach ($queryNotProgressAsset as $row) : ?>
                                             <tr>
                                                 <td><?= $row['unit_name'] ?></td>
-                                                <td><?= $row['product_type']=='system' ? '資通系統':'PC' ?></td>
+                                                <td><?= $row['product_type'] == 'system' ? '資通系統' : 'PC' ?></td>
                                                 <td><?= $row['asset_vendor'] ?></td>
                                                 <td><?= $row['asset_name'] ?></td>
                                                 <td><?= $row['asset_number'] ?></td>
